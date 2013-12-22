1968dfa31f84c5e4978de00bb2c109b884141ee0
RobertWebb38 <TheNyanKitty@icloud.com>
2013-12-22 15:37:26 -0800

    First Commit
    The first commit. Contains the first source of the plugin.

---
+++ SilenceMode/src/com/RobertWebb38/SilenceMode/SilenceMain.java
@@ -0,0 +1,45 @@
+package com.RobertWebb38.SilenceMode;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SilenceMain extends JavaPlugin implements Listener {
+	private boolean silencemode;
+	
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		if(label.equalsIgnoreCase("silence")) {
+			if(cs.hasPermission("silence.toggle")) {
+				if(silencemode) {
+					silencemode = false;
+					cs.sendMessage(ChatColor.GREEN + "Silece mode disabled!");
+					return true;
+				}
+				silencemode = true;
+				cs.sendMessage(ChatColor.RED + "Silence mode enabled!");
+				return true;
+				
+			}
+			cs.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("permission-message")));
+			return true;
+		}
+		return false;
+	}
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onChat(AsyncPlayerChatEvent event) {
+		if(silencemode) {
+			if(!event.getPlayer().hasPermission("silence.bypass")) {
+				event.setCancelled(true);
+				event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("silence-message")));
+			}
+		}
+	}
+}
